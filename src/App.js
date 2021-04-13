d3122574ddae7aa831d27b39e84b66d5e59d7dc8
JBoundless <39219944+JBoundless@users.noreply.github.com>
2021-04-13 07:25:03 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import React, { useState } from "react";
+import Axios from "axios";
+
+export default function App() {
+  const [joke, setJoke] = useState("");
+  const getJoke = () => {
+    Axios.get("https://official-joke-api.appspot.com/random_joke").then(
+      (response) => {
+        setJoke(response.data.setup + "..." + response.data.punchline);
+      }
+    );
+  };
+  return (
+    <div className="App">
+      <h1>The Joke's On You</h1>
+      <button onClick={getJoke}>Get joke right now</button>
+      <p>{joke}</p>
+    </div>
+  );
+}
